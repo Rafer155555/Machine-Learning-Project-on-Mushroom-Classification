f9f1d25c409536dfa1a7b064eaaa24c03f525fb6
Fernando Vivanco <75646374+Rafer155555@users.noreply.github.com>
2023-06-09 11:59:18 -0500

    Create PrintSchema.scala
    The schema lists all the columns in the DataFrame, along with their data types. In this case, all the columns are of type string. The nullable = true indicates that the columns allow null values.
    
    This information is helpful for understanding the structure and types of the data in the DataFrame.

---
+++ PrintSchema.scala
@@ -0,0 +1,30 @@
+scala
+
+mushroom.printSchema();
+
+Output:
+
+root
+ |-- class: string (nullable = true)
+ |-- capshape: string (nullable = true)
+ |-- capsurface: string (nullable = true)
+ |-- capcolor: string (nullable = true)
+ |-- bruises: string (nullable = true)
+ |-- odor: string (nullable = true)
+ |-- gillattachment: string (nullable = true)
+ |-- gillspacing: string (nullable = true)
+ |-- gillsize: string (nullable = true)
+ |-- gillcolor: string (nullable = true)
+ |-- stalkshape: string (nullable = true)
+ |-- stalkroot: string (nullable = true)
+ |-- stalksurfaceabovering: string (nullable = true)
+ |-- stalksurfacebelowring: string (nullable = true)
+ |-- stalkcolorabovering: string (nullable = true)
+ |-- stalkcolorbelowring: string (nullable = true)
+ |-- veiltype: string (nullable = true)
+ |-- veilcolor: string (nullable = true)
+ |-- ringnumber: string (nullable = true)
+ |-- ringtype: string (nullable = true)
+ |-- sporeprintcolor: string (nullable = true)
+ |-- population: string (nullable = true)
+ |-- habitat: string (nullable = true)
