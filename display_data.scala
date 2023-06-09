22f3e2bee7052e236af4f6418849e8ebe2e3e191
Fernando Vivanco <75646374+Rafer155555@users.noreply.github.com>
2023-06-09 12:36:32 -0500

    Create display_data.scala

---
+++ display_data.scala
@@ -0,0 +1,8 @@
+%scala
+
+val splits = mushroomDF.randomSplit(Array(0.7, 0.3))
+val train = splits(0)
+val test = splits(1)
+val train_rows = train.count()
+val test_rows = test.count()
+println("Training Rows: " + train_rows + " Testing Rows: " + test_rows)
