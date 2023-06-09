e1dc6e3abe241fccc4fa351b9d4fd602f60f48fa
Fernando Vivanco <75646374+Rafer155555@users.noreply.github.com>
2023-06-09 12:37:48 -0500

    Create train_a_classification_model.scala

---
+++ train_a_classification_model.scala
@@ -0,0 +1,6 @@
+%scala
+import org.apache.spark.ml.classification.LogisticRegression
+
+val lr = new LogisticRegression().setLabelCol("label").setFeaturesCol("features").setMaxIter(10).setRegParam(0.3)
+val model = lr.fit(training)
+println("Model Trained!")
