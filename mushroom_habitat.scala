167c5771ac1588bd7f72882490009cc5b69e7394
Fernando Vivanco <75646374+Rafer155555@users.noreply.github.com>
2023-06-09 12:30:04 -0500

    Create mushroom_habitat.scala

---
+++ mushroom_habitat.scala
@@ -0,0 +1,13 @@
+%sql
+
+select count(habitat), 
+CASE 
+	WHEN habitat = "g" THEN "grasses"
+	WHEN habitat = "l" THEN "leaves"
+	WHEN habitat = "m" THEN "meadows"
+	WHEN habitat = "p" THEN "paths"
+	WHEN habitat = "u" THEN "urban"
+	WHEN habitat = "w" THEN "waste"
+	ELSE "wood"
+END AS Habitat
+from MushroomData group by habitat
