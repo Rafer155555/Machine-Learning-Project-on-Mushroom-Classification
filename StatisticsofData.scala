75b0fdd974827b17435e9f7f9da144166ccdb520
Fernando Vivanco <75646374+Rafer155555@users.noreply.github.com>
2023-06-09 12:00:41 -0500

    Create StatisticsofData.scala

---
+++ StatisticsofData.scala
@@ -0,0 +1,15 @@
+scala
+
+mushroom.describe().show()
+
+Output:
+
++-------+-----+--------+----------+--------+-------+----+--------------+-----------+--------+---------+----------+---------+---------------------+---------------------+-------------------+-------------------+--------+---------+----------+--------+---------------+----------+-------+
+|summary|class|capshape|capsurface|capcolor|bruises|odor|gillattachment|gillspacing|gillsize|gillcolor|stalkshape|stalkroot|stalksurfaceabovering|stalksurfacebelowring|stalkcolorabovering|stalkcolorbelowring|veiltype|veilcolor|ringnumber|ringtype|sporeprintcolor|population|habitat|
++-------+-----+--------+----------+--------+-------+----+--------------+-----------+--------+---------+----------+---------+---------------------+---------------------+-------------------+-------------------+--------+---------+----------+--------+---------------+----------+-------+
+|  count| 8124|    8124|      8124|    8124|   8124|8124|          8124|       8124|    8124|     8124|      8124|     8124|                 8124|                 8124|               8124|               8124|    8124|     8124|      8124|    8124|           8124|      8124|   8124|
+|   mean| null|    null|      null|    null|   null|null|          null|       null|    null|     null|      null|     null|                 null|                 null|               null|               null|    null|     null|      null|    null|           null|      null|   null|
+| stddev| null|    null|      null|    null|   null|null|          null|       null|    null|     null|      null|     null|                 null|                 null|               null|               null|    null|     null|      null|    null|           null|      null|   null|
+|    min|    e|       b|         f|       b|      f|   a|             a|          c|       b|        b|         e|        ?|                    f|                    f|                  b|                  b|       p|        n|         n|       e|              b|         a|      d|
+|    max|    p|       x|         y|       y|      t|   y|             f|          w|       n|        y|         t|        r|                    y|                    y|                  y|                  y|       p|        y|         t|       p|              y|         y|      w|
++-------+-----+--------+----------+--------+-------+----+--------------+-----------+--------+---------+----------+---------+---------------------+---------------------+-------------------+-------------------+--------+---------+----------+--------+---------------+----------+-------+
