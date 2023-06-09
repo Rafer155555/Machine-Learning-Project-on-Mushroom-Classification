6e927216f9971ddc148030ad9deba7ef7f4fc165
Fernando Vivanco <75646374+Rafer155555@users.noreply.github.com>
2023-06-09 12:33:11 -0500

    Create collecting_all_string_columns_into_an_array.scala

---
+++ collecting_all_string_columns_into_an_array.scala
@@ -0,0 +1,3 @@
+scala
+
+var StringfeatureCol = Array("class", "capshape", "capsurface", "capcolor", "bruises", "odor", "gillattachment", "gillspacing", "gillsize", "gillcolor", "stalkshape", "stalkroot", "stalksurfaceabovering", "stalksurfacebelowring", "stalkcolorabovering", "stalkcolorbelowring", "veiltype", "veilcolor", "ringnumber", "ringtype", "sporeprintcolor", "population", "habitat")
